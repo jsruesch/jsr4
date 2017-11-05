9222fa51cc35c77acc46db4676397ee70e28aaf3
jsruesch <jake@gofarwell.com>
2017-11-05 12:04:07 -0600

    Add more CSC logos

--- project_csc.php
+++ project_csc.php
@@ -1,5 +1,5 @@
 <?php include('header.php') ?>
-<?php include('navigation.php') ?>  
+<?php include('navigation.php') ?>
 
 
 <div class="mc">
@@ -7,23 +7,23 @@
     <div class="wrap center">
 
         <div class="project_title_container">
-            
+
             <div class="project_title">
                 <p class="project_header">Core Skill Clinic</p>
             </div>
-            
+
             <div class="project_categories">
                 <p>Branding, Vector</p>
             </div>
-            
+
         </div>
-            
+
         <div class="content">
             <img class="full" src="images/project_images/csc/csc_full.png"/>
         </div>
-            
+
         <div class="content">
-            
+
             <div class="full">
 
                 <h2 class="header center_text">Hockeee!</h2>
@@ -37,13 +37,13 @@
                     <a href="http://www.sunprairieice.com/core-skills-clinic" target="_blank">
                         visit our page on the Sun Prairie Ice Arena site</a>.
                 </p>
-                
+
             </div>
 
         </div>
 
         <div class="content">
-            
+
             <div class="half left pright">
                 <p class="tteal bold">THE HAMMERSNOW CUP</p><br />
                 <p>Some of our skaters have been and may never have the chance to play on the big stage for something
@@ -52,13 +52,13 @@
                 scorekeepers, announcers, medals, an MVP trophy, and the chance to get your name permanantely engrained
                 on the Legendary Hammersnow Cup of Eternal Glory.<br /><br /></p>
             </div>
-            
+
             <div class="half right">
                 <img class="full" src="images/project_images/csc/csc_01.png"/>
             </div>
-            
+
         </div>
-        
+
         <div class="content">
             <p>I design the logos for our jerseys and then a good buddy of mine screen prints them on jerseys for us. The
             challenge here is that I'm limited to six colors maximum. It's tough for a detail freak like me to do that but
@@ -66,80 +66,104 @@
             for us each session!
             </p>
         </div>
-        
+
         <div class="content">
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_pirates.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_jaeger.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_tendies.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_ninjas.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_kaiju.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_dragons.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_resistance.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_first_order.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_boba_tendies.png"/>
             </div>
-            
+
+            <div class="one_third left">
+                <img class="full" src="images/project_images/csc/csc_captain_america.png"/>
+            </div>
+
+            <div class="one_third left">
+                <img class="full" src="images/project_images/csc/csc_iron_man.png"/>
+            </div>
+
+            <div class="one_third left">
+                <img class="full" src="images/project_images/csc/csc_carnage.png"/>
+            </div>
+
+            <div class="one_third left">
+                <img class="full" src="images/project_images/csc/csc_venom.png"/>
+            </div>
+
+            <div class="one_third left">
+                <img class="full" src="images/project_images/csc/csc_iron_spider.png"/>
+            </div>
+
         </div>
-            
-        <div class="content">    
-            <p>The Kaiju logo was based on
-            <a href="http://marcus-dublin.deviantart.com/art/Kaiju-Titanus-DawnApproach-Render-424367047" target="_blank">
-                this amazing Kaiju sculpt by Marcus Dublin</a> and the Jaeger was based on a statue of Striker Eureka I own.
+
+        <div class="content">
+            <p>
+              Inspiration credits:
+              <a href="http://marcus-dublin.deviantart.com/art/Kaiju-Titanus-DawnApproach-Render-424367047" target="_blank">Kaiju</a>,
+              <a href="https://secure.parksandresorts.wdpromedia.com/media/disneyparks/blog/wp-content/uploads/2013/08/cpt177577LARGE.jpg" target="_blank">Captain America</a>,
+              <a href="http://spiderman.wikia.com/wiki/File:Venom_II_(MacDonald_Gargan).jpg" target="_blank">Venom</a>,
+              <a href="https://static.comicvine.com/uploads/original/11122/111221607/4481486-3664545264-carna.jpg" target="_blank">Carnage</a>,
+              <a href="https://vignette.wikia.nocookie.net/ironman/images/1/11/Photo%28424%29.JPG/revision/latest?cb=20130713161257" target="_blank">Iron Spider</a>
+            </p>
         </div>
-        
+
         <div class="content">
             <p class="tteal bold">DID I MENTION THIS IS AN INSANE AMOUNT OF FUN?<br /><br /></p>
-            
+
             <div class="half left">
                 <img class="full" src="images/project_images/csc/csc_02.png"/>
             </div>
-            
+
             <div class="half right">
                 <img class="full" src="images/project_images/csc/csc_03.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_04.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_05.png"/>
             </div>
-            
+
             <div class="one_third left">
                 <img class="full" src="images/project_images/csc/csc_06.png"/>
             </div>
-            
+
         </div>
-            
+
     </div>
 
 </div>
 
-<?php include('footer.php') ?> 
-
+<?php include('footer.php') ?>
